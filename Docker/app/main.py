92c48e4ac968ac94c1016225cf6a78bfc7f593e9
Alexander D <alexander.dominicus@rub.de>
2024-10-02 15:01:37 +0200

    Added Docker

---
+++ Docker/app/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from openai import OpenAI
+
+
+import os
+
+
+# Modell für die Anfrage (Pydantic-Modell)
+class TextRequest(BaseModel):
+    text: str
+
+# API KEY
+client = OpenAI(
+    api_key = "...",
+)
+
+
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "I.O"}
+
+# Route für POST-Anfragen
+@app.post("/receive_text")
+def receive_text(text_request: TextRequest):
+    received_text = text_request.text
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+   # thread_id="thread_zJ98dJMpA4lmBO8fP6z8l92G",
+    role="user",
+    content=received_text)
+    run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id,
+    assistant_id="....",
+    instructions=""
+    )
+
+    if run.status == 'completed':
+        messages = client.beta.threads.messages.list(
+        thread_id=thread.id)
+        answer= messages.data[0].content[0].text.value
+   
+    return {"received_text": answer}
+
+origins = [
+   "https://<your_origin_here>"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+	allow_headers=["*"],
+    max_age=3600,
+)
